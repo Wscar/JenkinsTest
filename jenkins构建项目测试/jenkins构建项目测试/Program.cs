d85bcb7733e590cac919ce6f2e67b40f005801a3
yemobai <546307885@qq.com>
2019-02-20 15:48:52 +0800

    添加了项目

---
+++ jenkins构建项目测试/jenkins构建项目测试/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace jenkins构建项目测试
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
